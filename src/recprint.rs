7034b66d501745241c7c1d231f5af5bb26fd0147
naens <naens@gmx.com>
2018-10-12 20:14:14 +0200

    objrec: add segdef

--- src/recprint.rs
+++ src/recprint.rs
@@ -38,7 +38,67 @@ pub fn lnames(orec: ObjectRecord) {
     println!();
 }
 
-pub fn segdef(orec: ObjectRecord) { tmp(orec) }
+pub fn segdef(orec: ObjectRecord) {
+    println!("Segment Definition Record (SEGDEF)");
+    println!("=================================");
+    let acbp = orec.data[0];
+    let a = (acbp & 0xe0) >> 5;
+    let c = (acbp & 0x1c) >> 2;
+    let b1 = (acbp & 0x02) >> 1;               /* b1 because of joe bug */
+    let p = acbp & 0x01;
+
+    let alignment = match a {
+        0 => "absolute",
+        1 => "relocatable, byte aligned",
+        2 => "relocatable, word aligned",
+        3 => "relocatable, paragraph (16-byte) aligned",
+        4 => "relocatable, aligned on a page boundary",
+        5 => "relocatable, double-word aligned",
+        6 => "not supported",
+        7 => "not definied",
+        _ => "<bad value>"
+    };
+    println!("Alignment: {}", alignment);
+
+    let combination = match c {
+        0 => "private",
+        1 | 6 => "common",
+        2 | 4 | 7 => "public",
+        5 => "stack",
+        _ => "<bad value>"
+    };
+    println!("Combination: {}", combination);
+
+    println!("Big: {}", if b1 == 1 { "true" } else {"false" });
+
+    println!("P: {}", if p == 1 { "true" } else {"false" });
+
+    /* frame number and offset when A is 0 */
+    let i;
+    if a == 0 {
+        let frame_number = (orec.data[1] as u32) + 256*(orec.data[2] as u32);
+        let offset = orec.data[3];
+        println!("Frame number: {}", frame_number);
+        println!("Offset: {}", offset);
+        i = 4;
+    } else {
+        i = 1;
+    }
+    let segment_length = (orec.data[i] as u32) + 256*(orec.data[i+1] as u32);
+    println!("Segment length: {}", segment_length);
+
+    let segment_name_index = orec.data[i+2];
+    println!("Segment name index: {}", segment_name_index);
+
+    let class_name_index = orec.data[i+3];
+    println!("Class name index: {}", class_name_index);
+
+    let overlay_name_index = orec.data[i+4];
+    println!("Overlay name index: {}", overlay_name_index);
+
+    println!();
+}
+
 pub fn grpdef(orec: ObjectRecord) { tmp(orec) }
 pub fn fixupp(orec: ObjectRecord) { tmp(orec) }
 pub fn ledata(orec: ObjectRecord) { tmp(orec) }
