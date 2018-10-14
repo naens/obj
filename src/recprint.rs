f4de760244ecd9b3b534157fe94c1e5b82917a3f
naens <naens@gmx.com>
2018-10-14 11:14:05 +0200

    recprint: fix modend, improve fixupp

--- src/recprint.rs
+++ src/recprint.rs
@@ -2,17 +2,69 @@ use std::str;
 
 use ::objrec::*;
 
+fn read_u16(vec: &[u8]) -> u16 {
+    (vec[0] as u16) + 0x100 * (vec[1] as u16)
+}
+
+/* prints the dat info and returns the amount of bytes read */
+fn print_dat(vec: &[u8]) -> usize {
+    let mut i: usize = 0;
+    let dat = vec[i];
+    i = i + 1;
+    let f = dat >> 7;
+    print!("\tFrame: ");
+    if f == 1 {
+        let frame_thread = (dat >> 4) & 7;
+        print!("thread={} ", frame_thread);
+    } else {
+        let frame_method = (dat >> 4) & 7;
+        print!("method={} ", frame_method);
+        if frame_method != 5 {
+            let frame_datum = vec[i];
+            print!(", datum={}", frame_datum);
+            i = i + 1;
+        }
+    }
+    println!();
+    print!("\tTarget: ");
+    let t = (dat >> 3) & 1;
+    if t == 1 {
+        let target_thread = dat & 3;
+        print!("thread={}", target_thread);
+    } else {
+        let target_datum = vec[i];
+        print!("datum={}, ", target_datum);
+        i = i + 1;
+        let target_method = dat & 7;
+        print!("method={} ", target_method);
+        if target_method < 3 {
+            let target_displacement = read_u16(&vec[i..]);
+            print!("displacement={}", target_displacement);
+            i = i + 2;
+        }
+    }
+    println!();
+    return i;
+}
+
 pub fn regint(orec: ObjectRecord) {
     println!("Register Initialization Record (REGINT)");
     println!("=======================================");
-//    let mut i = 0;
-//    while i < orec.data.len() {
+    let mut i = 0;
+    while i < orec.data.len() {
+        println!("regtyp={:02x}", orec.data[i]);
         let regid = orec.data[0] >> 6;
+        i = i + 1;
         let l = orec.data[0] & 1;
         println!("regid={}, l={}", regid, l);
-        /* TODO: fixdat, enddat, regdat function (fixupp, modend, regint) */
-//        i = i + ...;
-//    }
+        if l == 1 {
+            println!("regdat={:02x}", orec.data[i]);
+            i = i + print_dat(&orec.data[i..]);
+            println!("i={}", i);
+        } else {
+            panic!("regint, l=0: not implemented");
+        }
+    }
     println!();
 }
 
@@ -43,39 +95,7 @@ pub fn modend(orec: ObjectRecord) {
         if module_type & 1 == 1 {
             println!("Start address contains a relocatable address reference");
         }
-        let end_data = orec.data[1];
-        let mut i = 2;
-        /* TODO: separate regdat/enddat/fixdat function */
-        print!("\tFrame: ");
-        if end_data & 0x80 != 0 {	/* frame specified explicitly */
-            let frame_thread = (end_data >> 4) & 7;
-            print!("thread={} ", frame_thread);
-        } else {			/* frame in thread */
-            let frame_method = (end_data >> 4) & 7;
-            print!("method={} ", frame_method);
-            if frame_method != 5 {
-                let frame_datum = orec.data[i];
-                print!(", datum={}", frame_datum);
-                i = i + 1;
-            }
-        }
-        println!();
-        print!("\tTarget: ");
-        if end_data & 0x08 != 0 {	/* target specified explicitly */
-            let target_thread = end_data & 3;
-            print!("thread={}", target_thread);
-        } else {			/* target in thread */
-            let target_datum = orec.data[i];
-            print!("datum={}, ", target_datum);
-            i = i + 1;
-            let target_method = end_data & 7;
-            print!("method={} ", target_method);
-            if target_method < 3 {
-                let target_displacement = (orec.data[i] as u32)
-                        + 256*(orec.data[i+1] as u32);
-                print!("displacement={}", target_displacement);
-            }
-        }
+        print_dat(&orec.data[1..]);
     }
     println!();
 }
@@ -113,7 +133,7 @@ pub fn pubdef(orec: ObjectRecord) {
 
     let i;
     if base_segment_index == 0 {
-        let base_frame = (orec.data[2] as u32) + 256*(orec.data[3] as u32);
+        let base_frame = read_u16(&orec.data[2..]);
         println!("Base frame: {}", base_frame);
         i = 4;
     } else {
@@ -124,7 +144,7 @@ pub fn pubdef(orec: ObjectRecord) {
     while !p.is_empty() {
         let length = p[0] as usize;
         let name = str::from_utf8(&p[1..length+1]).unwrap();
-        let public_offset = (p[length+1] as u32) + 256*(p[length+2] as u32);
+        let public_offset = read_u16(&p[length+1..]);
         let type_index = p[length+3] as usize;
         print!("Name: {}, public offset: {}", name, public_offset);
         if type_index > 0 {
@@ -148,8 +168,8 @@ pub fn linnum(orec: ObjectRecord) {
 
     let mut i = 2;
     while i < orec.data.len() {
-        let line = (orec.data[i] as u32) + 256 * (orec.data[i+1] as u32);
-        let offset = (orec.data[i+2] as u32) + 256 * (orec.data[i+3] as u32);
+        let line = read_u16(&orec.data[i..]);
+        let offset = read_u16(&orec.data[i+2..]);
         println!("Line: {}, offset: {}", line, offset);
         i = i + 4;
     }
@@ -210,7 +230,7 @@ pub fn segdef(orec: ObjectRecord) {
     /* frame number and offset when A is 0 */
     let i;
     if a == 0 {
-        let frame_number = (orec.data[1] as u32) + 256*(orec.data[2] as u32);
+        let frame_number = read_u16(&orec.data[1..]);
         let offset = orec.data[3];
         println!("Frame number: {}", frame_number);
         println!("Offset: {}", offset);
@@ -218,7 +238,7 @@ pub fn segdef(orec: ObjectRecord) {
     } else {
         i = 1;
     }
-    let segment_length = (orec.data[i] as u32) + 256*(orec.data[i+1] as u32);
+    let segment_length = read_u16(&orec.data[i..]);
     println!("Segment length: {}", segment_length);
 
     let segment_name_index = orec.data[i+2];
@@ -244,6 +264,7 @@ pub fn fixupp(orec: ObjectRecord) {
         if orec.data[i] & 0x80 != 0 {	/* fixup field */
             print!("Fixup field: ");
             let locat = (orec.data[i+1] as u32) + 256*(orec.data[i] as u32);
+            i = i + 2;
             let m = (locat >>14 ) & 1;
             let loc = (locat >> 10) & 7;
             let data = locat & 0x3ff;
@@ -253,44 +274,7 @@ pub fn fixupp(orec: ObjectRecord) {
                 print!("self-relative, ");
             }
             println!("loc={}, data={:03x}", loc, data);
-
-            let fixdat = orec.data[i+2];
-            i = i + 3;
-            let f = fixdat >> 7;
-            /* fixdat: separate function */
-            print!("\tFrame: ");
-            if f == 1 {
-                let frame_thread = (fixdat >> 4) & 7;
-                print!("thread={} ", frame_thread);
-            } else {
-                let frame_method = (fixdat >> 4) & 7;
-                print!("method={} ", frame_method);
-                if frame_method != 5 {
-                    let frame_datum = orec.data[i];
-                    print!(", datum={}", frame_datum);
-                    i = i + 1;
-                }
-            }
-            println!();
-            print!("\tTarget: ");
-            let t = (fixdat >> 3) & 1;
-            if t == 1 {
-                let target_thread = fixdat & 3;
-                print!("thread={}", target_thread);
-            } else {
-                let target_datum = orec.data[i];
-                print!("datum={}, ", target_datum);
-                i = i + 1;
-                let target_method = fixdat & 7;
-                print!("method={} ", target_method);
-                if target_method < 3 {
-                    let target_displacement = (orec.data[i] as u32)
-                            + 256*(orec.data[i+1] as u32);
-                    print!("displacement={}", target_displacement);
-                    i = i + 2;
-                }
-            }
-            println!();
+            i = i + print_dat(&orec.data[i..]);
         } else {		/* thread field */
             let data = orec.data[i];
             let number = data & 3;
@@ -313,7 +297,7 @@ pub fn ledata(orec: ObjectRecord) {
     let segment_index = orec.data[0];
     println!("Segment index: {}", segment_index);
 
-    let data_offset = (orec.data[1] as u32) + 256*(orec.data[2] as u32);
+    let data_offset = read_u16(&orec.data[1..]);
     println!("Enumerated data offset: {}", data_offset);
 
     let mut i = 0;
